ce19bc167d1ceaa923928619709b76d01e148d1b
3170116 <41377852+3170116@users.noreply.github.com>
2019-06-20 16:33:26 +0300

    Update getNumberOfDifferentPaths.py

--- getNumberOfDifferentPaths.py
+++ getNumberOfDifferentPaths.py
@@ -10,19 +10,19 @@ def getNumberOfDifferentPaths(V,E,s,t,K):
  #opt(i,j,1) = 0 if there is not the (i,j) edge else 1
  for i in range(len(V)):
    for j in range(len(V)):
-     opt(i,j,1) = 0
-     if tuple(V(i),V(j)) in E:
-       opt(i,j,1) = 1
+     opt[i][j][1] = 0
+     if tuple(V[i],V[j]) in E:
+       opt[i][j][1] = 1
  
   for k in range(2,K+1):
     for i in range(len(V)):
       for j in range(len(V)):
-        opt(i,j,k) = 0
-        for v in V:
-          if tuple(v,V(j)) in E:
-            opt(i,j,k) += opt(i,v,k-1)
+        opt[i][j][k] = 0
+        for v in range(len(V)):
+          if opt[v][j][1] == 1:#if tuple(v,V(j)) in E:
+            opt[i][j][k] += opt[i][v][k-1]
           
   for i in range(len(V)):
     for j in range(len(V)):
-      if tuple(V(i),V(j)) == tuple(s,t):
-        return opt(i,j,k)
+      if set(V[i],V[j]) == set(s,t):
+        return opt[i][j][k]
